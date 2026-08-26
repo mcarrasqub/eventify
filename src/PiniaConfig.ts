9aee838ce0b421890718b5d4ea5cb00aeb96531e
sgallo <sofiagdelarosa@gmail.com>
2026-08-26 14:12:31 -0500

    Adding pinia configuration

---
+++ src/PiniaConfig.ts
@@ -0,0 +1,35 @@
+import { createPinia } from 'pinia';
+import { watch } from 'vue';
+import { ticketSeeder } from '@/stores/ticketseeder.js';
+
+export default class PiniaConfig {
+  public static init() {
+    const pinia = createPinia();
+
+    const savedState = localStorage.getItem('piniaState');
+    if (savedState) {
+      pinia.state.value = JSON.parse(savedState);
+    } else {
+      // initialize the state with the seeders
+      pinia.state.value = {
+        ticket: {
+          tickets: ticketSeeder,
+        },
+      };
+
+      // save the initial state to localStorage
+      localStorage.setItem('piniaState', JSON.stringify(pinia.state.value));
+    }
+
+    // watch for changes and save to localStorage
+    watch(
+      pinia.state,
+      (state) => {
+        localStorage.setItem('piniaState', JSON.stringify(state));
+      },
+      { deep: true },
+    );
+
+    return pinia;
+  }
+}
